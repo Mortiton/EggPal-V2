b2d7c7f8c893dac25e5c3797deefe6dddd945d7e
Zoe F <zoe.e.foxley@gmail.com>
2024-06-06 00:41:03 +0100

    Update comments on route

--- src/app/auth/signout/route.js
+++ src/app/auth/signout/route.js
@@ -2,6 +2,14 @@ import { createClient } from '@/app/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
 import { NextResponse } from 'next/server'
 
+/**
+ * POST function to handle POST requests.
+ * It checks if a user is logged in. If a user is logged in, it signs out the user.
+ * After signing out the user, it revalidates the cache for the root path and redirects to the root path.
+ *
+ * @param {Object} req - The request object.
+ * @returns {Promise<NextResponse>} A promise that resolves to a NextResponse object.
+ */
 export async function POST(req) {
   const supabase = createClient()
 
@@ -10,11 +18,15 @@ export async function POST(req) {
     data: { user },
   } = await supabase.auth.getUser()
 
+  // If a user is logged in, sign out the user
   if (user) {
     await supabase.auth.signOut()
   }
 
+  // Revalidate the cache for the root path
   revalidatePath('/', 'layout')
+
+  // Redirect to the root path
   return NextResponse.redirect(new URL('/', req.url), {
     status: 302,
   })
