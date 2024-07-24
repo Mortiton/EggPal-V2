f2ba603bbe0e19b53d88b223f73eca704789051e
Zoe F <zoe.e.foxley@gmail.com>
2024-07-24 18:37:29 +0100

    Modify workIcon to user images in database

--- src/app/components/WorkIcon.js
+++ src/app/components/WorkIcon.js
@@ -3,36 +3,34 @@ import Image from 'next/image';
 import styles from './styles/WorkIcon.module.css'; // Import styles specific to the WorkIcon
 
 /**
- * WorkIcon component that renders an icon for a work.
+ * WorkIcon component that renders an icon for a work skill.
  * It only renders the icon if the value is greater than 0.
  *
  * @component
- * @param {string} iconName - The name of the icon to render.
- * @param {number} value - The value associated with the icon.
+ * @param {Object} props - The props that were defined by the caller of this component.
+ * @param {string} props.iconUrl - The URL of the icon to render.
+ * @param {number} props.value - The value associated with the icon.
  * @returns {JSX.Element|null} A React component, or null if the value is 0 or negative.
  */
-export default function WorkIcon({ iconName, value }) {
-    // Only render the component if the value is greater than 0
+export default function WorkIcon({ iconUrl, value }) {
     if (value <= 0) {
-        return null; // Return null to avoid rendering anything for this component if the value is 0 or negative
+      return null;
     }
-
-    const iconPath = `/images/work/${iconName}.png`; // Construct the path for the icon image
-
-    // Render the icon and its associated value
+  
     return (
-        <div className={styles.iconContainer} role="img" aria-label={`${iconName}: ${value}`}>
-            <Image 
-                src={iconPath} 
-                alt={`${iconName} icon`} 
-                width={25} 
-                height={25} 
-                className={styles.iconImage} 
-                priority 
-            />
-            <span className={styles.iconValue}>{value}</span>
-        </div>
+      <div className={styles.iconContainer} role="img" aria-label={`Icon: ${value}`}>
+        <Image
+          src={iconUrl}
+          alt={`Icon`}
+          width={25}
+          height={25}
+          className={styles.iconImage}
+          priority
+        />
+        <span className={styles.iconValue}>{value}</span>
+      </div>
     );
-}
-
-WorkIcon.displayName = 'WorkIcon'
+  }
+  
+  
+  WorkIcon.displayName = 'WorkIcon';
