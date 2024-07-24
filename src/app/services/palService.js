cb84d4848dca06f732e8dac92f39db5fdf4e38db
Zoe F <zoe.e.foxley@gmail.com>
2024-07-24 19:26:33 +0100

    Implement react cache for getpals

--- src/app/services/palService.js
+++ src/app/services/palService.js
@@ -1,31 +1,51 @@
+
+
+import React from 'react';
 import { createClient } from '../utils/supabase/server';
 
-/**
- * Fetches and combines pal information, icon URLs, and skills data.
- *
- * @param {string[]} [ids=[]] - Array of pal IDs to fetch.
- * @returns {Promise<Object[]>} - A promise that resolves to an array of combined pal data objects.
- * @throws Will throw an error if the request fails.
- */
-export async function getPals(ids = []) {
-    const supabase = createClient();
-    console.log('Fetching pals data with IDs:', ids);
-  
-    try {
-      const { data, error } = await supabase.rpc('get_pals', { ids: ids.length ? ids : null });
-  
-      if (error) {
-        console.error('Supabase RPC Error:', error);
-        throw new Error(`Error fetching pals: ${error.message}`);
-      }
-  
-      console.log('Number of pals fetched:', data.length);
-      return data;
-    } catch (error) {
-      console.error('Error fetching pals:', error.message);
-      return null;
-    }
+const fetchPals = async (ids = []) => {
+  const supabase = createClient();
+  const { data, error } = await supabase.rpc('get_pals', { ids: ids.length ? ids : null });
+
+  if (error) {
+    throw new Error(`Error fetching pals: ${error.message}`);
   }
+
+  return data;
+};
+
+const cachedFetchPals = React.cache(fetchPals);
+
+export async function getPals(ids = []) {
+  return cachedFetchPals(ids);
+}
+
+// /**
+//  * Fetches and combines pal information, icon URLs, and skills data.
+//  *
+//  * @param {string[]} [ids=[]] - Array of pal IDs to fetch.
+//  * @returns {Promise<Object[]>} - A promise that resolves to an array of combined pal data objects.
+//  * @throws Will throw an error if the request fails.
+//  */
+// export async function getPals(ids = []) {
+//     const supabase = createClient();
+//     console.log('Fetching pals data with IDs:', ids);
+  
+//     try {
+//       const { data, error } = await supabase.rpc('get_pals', { ids: ids.length ? ids : null });
+  
+//       if (error) {
+//         console.error('Supabase RPC Error:', error);
+//         throw new Error(`Error fetching pals: ${error.message}`);
+//       }
+  
+//       console.log('Number of pals fetched:', data.length);
+//       return data;
+//     } catch (error) {
+//       console.error('Error fetching pals:', error.message);
+//       return null;
+//     }
+//   }
 
   /**
  * Fetches all unique work types and their corresponding icons from the icons table.
